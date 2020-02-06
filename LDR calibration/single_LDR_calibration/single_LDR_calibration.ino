86feca1ece27c8ba2b77b149b2854b95c935715d
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-02-06 21:49:21 +0800

    Create single_LDR_calibration.ino

---
+++ LDR calibration/single_LDR_calibration/single_LDR_calibration.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  delay(100);
+  Serial.println(voltage);
+
+}
