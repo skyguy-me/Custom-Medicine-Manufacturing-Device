c424949730c68ecf70003f76d2fda060ca1f7ed5
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-02-07 14:21:19 +0800

    Update intialServoTest.ino

--- CustomServo_Test/intialServoTest/intialServoTest.ino
+++ CustomServo_Test/intialServoTest/intialServoTest.ino
@@ -1,6 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
   pinMode(2, OUTPUT);
 }
 
@@ -8,4 +8,10 @@ void loop() {
 
   // put your main code here, to run repeatedly:
   analogWrite(2, 255);
+  Serial.println("running");
+  delay(2500);
+  analogWrite(2, 0);
+  Serial.println("stopped");
+  delay(2500);
+
 }
