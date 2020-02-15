05b8b734b92b8a4ef236223e77fcbb6824e2d1ee
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-02-16 03:17:40 +0800

    Update multiple_LDR_calibration.ino

--- LDR calibration/multiple_LDR_calibration/multiple_LDR_calibration/multiple_LDR_calibration.ino
+++ LDR calibration/multiple_LDR_calibration/multiple_LDR_calibration/multiple_LDR_calibration.ino
@@ -1,9 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  String stringOne = "A long integer: ";
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  int sensorValue = analogRead(A0);
+
+  float voltage = sensorValue * (5.0 / 1023.0);
+  delay(100);
+  Serial.println(voltage);
 }
