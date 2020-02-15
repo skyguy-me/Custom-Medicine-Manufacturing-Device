f9a9d190435903abe83e964e8514bed69f205afd
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-02-16 03:24:52 +0800

    Update multiple_LDR_calibration.ino

--- LDR calibration/multiple_LDR_calibration/multiple_LDR_calibration/multiple_LDR_calibration.ino
+++ LDR calibration/multiple_LDR_calibration/multiple_LDR_calibration/multiple_LDR_calibration.ino
@@ -1,15 +1,32 @@
+String stringTitle = "A1,A2,A3,A4,A5,A7,A8,A8,A10";
+String stringValue = "";
+
+int sensorCount = 10;
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  String stringOne = "A long integer: ";
+
+
 
 }
 
 void loop() {
 
-  int sensorValue = analogRead(A0);
+  int index = 0;
+  int sensorValue;
+  float voltage;
 
-  float voltage = sensorValue * (5.0 / 1023.0);
+  for(index = 0;index<sensorCount;index++){  voltage = sensorValue * (5.0 / 1023.0);
+}
+
+
+
+
+
+  stringValue += voltage.ToString() + ",";
   delay(100);
   Serial.println(voltage);
 }
