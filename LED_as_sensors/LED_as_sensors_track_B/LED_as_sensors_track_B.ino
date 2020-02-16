4160338fae73345b8cae209f06407d406dbabfdb
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-02-16 12:57:15 +0800

    Update LED_as_sensors_track_B.ino

--- LED_as_sensors/LED_as_sensors_track_B/LED_as_sensors_track_B.ino
+++ LED_as_sensors/LED_as_sensors_track_B/LED_as_sensors_track_B.ino
@@ -17,33 +17,42 @@ int resetsamples = 30;          // how many cycles to run the light on before yo
 void setup() {
   pinMode(led, OUTPUT);
   Serial.begin(9600);                                   // no real part of the program, just for debugging
-  for(teller =0; teller < samples; teller++) {// remember the lowest value out of many readings
-   sens = analogRead(sensorpin);
-   if (sens < basis){basis = sens;}
-   delay(vertraging);                                     // the sensor needs a delay here to catch its breath
+  for (teller = 0; teller < samples; teller++) { // remember the lowest value out of many readings
+    sens = analogRead(sensorpin);
+    if (sens < basis) {
+      basis = sens;
+    }
+    delay(vertraging);                                     // the sensor needs a delay here to catch its breath
   }
 }
 
 
-void loop() {                                            
-for(teller =0; teller < samples; teller++) {// remember the lowest value out of many readings
-   sens = analogRead(sensorpin);
-   delay(vertraging);                                   // the sensor needs a delay here to catch its breath
-   if (sens < test){
-   test3 = sens;                                            // remember the 3 lowest readings
-   test2 = test3;
-   test = test2;}
+void loop() {
+  for (teller = 0; teller < samples; teller++) { // remember the lowest value out of many readings
+    sens = analogRead(sensorpin);
+    delay(vertraging);                                   // the sensor needs a delay here to catch its breath
+    if (sens < test) {
+      test3 = sens;                                            // remember the 3 lowest readings
+      test2 = test3;
+      test = test2;
+    }
   }
-  if (test < basis-marge && test2 < basis-marge && test3 < basis-marge){//all 3 low readings mus be < the basis reading
+  if (test < basis - marge && test2 < basis - marge && test3 < basis - marge) { //all 3 low readings mus be < the basis reading
     digitalWrite(led, LOW);
     resetteller++;                                         // count how long the LED stays on
   }
-    else{
-      digitalWrite(led, HIGH);
-      basis = test;                                         // if the lowest test reading is higher than the basis, basis will be reset
-      resetteller = 0;
-    }
-    if (resetteller > resetsamples){basis = test;}//if LED stays on to long, we don't trust it and reset basis
-    Serial.print(basis);Serial.print("  ");Serial.print(test);Serial.print("  ");Serial.println(sens);//just for debugging
-    test = 1024;
+  else {
+    digitalWrite(led, HIGH);
+    basis = test;                                         // if the lowest test reading is higher than the basis, basis will be reset
+    resetteller = 0;
+  }
+  if (resetteller > resetsamples) {
+    basis = test; //if LED stays on to long, we don't trust it and reset basis
+  }
+  // Serial.print(basis);
+  //Serial.print("  ");
+  Serial.println(test);
+  //Serial.print("  ");
+  //Serial.println(sens);//just for debugging
+  test = 1024;
 }
