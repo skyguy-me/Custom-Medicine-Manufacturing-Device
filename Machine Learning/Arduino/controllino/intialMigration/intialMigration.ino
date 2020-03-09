f7c1e9c89ad9a150ab64791f30df9e2633388920
Gokul <44331390+skyguy-me@users.noreply.github.com>
2020-03-09 18:01:36 +0800

    Update intialMigration.ino

--- Machine Learning/Arduino/controllino/intialMigration/intialMigration.ino
+++ Machine Learning/Arduino/controllino/intialMigration/intialMigration.ino
@@ -21,69 +21,15 @@ void setup() {
   Serial.begin(2000000);
 
 
+ 
+}
+
+void loop() {
   while (!CodeStop) {
     float time = micros();
-    while (i < 100) {
-      //      // put your main code here, to run repeatedly:
-      //      // read the input on analog pin:
-      //
-      //      Serial.println("i=" + String(i));
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A0);
-      //      //Serial.print("i=" + String(i) + "  " + String(dataFrame[1][i][j]));
-      //      j++;
-      //
-      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A1);
-      //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A2);
-      //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A3);
-      //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //      //
-      //      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A4);
-      //      // Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A5);
-      //      //   Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A6);
-      //      //Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A7);
-      //      // Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A8);
-      //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A9);
-      //      //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //      //
-      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A10);
-      //      //      //Serial.print(" ," + String(dataFrame[1][i][j]));
-      //      j++;
-      //      //
-      //      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A9);
-      //      //      // Serial.print(" ," + String(dataFrame[1][i][j]) + "\n");
-      //
-      //      i = i + 1;
-      //      j = 0;
-      //      // print out the value you read:
-      //    }
-      //    //    Serial.println(time = micros() - time  );
-      //    //
-      for (i = 0; i < 100; i++) {
+    while (i < 1000) {
+    
+      for (i = 0; i < 1000; i++) {
         for (j = 0; j < 12; j++) {
           switch (j) {
             case 0:   Serial.print(String(analogRead(CONTROLLINO_A0)));
@@ -118,86 +64,10 @@ void setup() {
       }
       //    i = 0;
 
-          Serial.println("time to complete = " + String(time = micros() - time) +"microseconds" );
+          Serial.println("time to complete = " + String(time = micros() - time) +" microseconds" );
 
 
     }
   }
-}
-
-void loop() {
-  //  while (!CodeStop) {
-  //    float time = micros();
-  //    while (i < 100) {
-  //      // put your main code here, to run repeatedly:
-  //      // read the input on analog pin:
-  //
-  //      Serial.println("i=" + String(i));
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A0);
-  //      //Serial.print("i=" + String(i) + "  " + String(dataFrame[1][i][j]));
-  //      j++;
-  //
-  //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A1);
-  //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //      //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A2);
-  //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //      //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A3);
-  //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //      //
-  //      //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A4);
-  //      // Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //      //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A5);
-  //      //   Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A6);
-  //      //Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A7);
-  //      // Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A8);
-  //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //      //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A9);
-  //      //      //  Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //      //
-  //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A10);
-  //      //      //Serial.print(" ," + String(dataFrame[1][i][j]));
-  //      j++;
-  //      //
-  //      //      dataFrame[1][i][j] = analogRead(CONTROLLINO_A9);
-  //      //      // Serial.print(" ," + String(dataFrame[1][i][j]) + "\n");
-  //
-  //      i = i + 1;
-  //      j = 0;
-  //      // print out the value you read:
-  //    }
-  //    Serial.println(time = micros() - time  );
-  //
-  //        for (x = 0; x < i; x++) {
-  //          for (y = 0; y < 12; y++) {
-  //            Serial.print(" ," + String(dataFrame[1][x][y]));
-  //          }
-  //          Serial.print("\n");
-  //        }
-  //        //    i = 0;
-  //    //
-  //    //    Serial.println("\n\n");
-  //
-  //  }
 
 }
