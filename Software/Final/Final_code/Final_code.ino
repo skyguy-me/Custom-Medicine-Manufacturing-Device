b4e3ec57dd43921b021cf037af5539af2c75c816
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-03-09 23:12:02 +0800

    serial event

--- Software/Final/Final_code/Final_code.ino
+++ Software/Final/Final_code/Final_code.ino
@@ -1,11 +1,21 @@
+#include <SPI.h>
+
+
 
 //state machine
 bool waitStage = true;
 bool feederStage = false;
 bool scanStage = false;
+bool verificationStage = false;
 bool packStage = false;
 
 
+// int MOSI = 51;
+// int MISO = 50;
+// int SCK = 52;
+// int RESET =  48; //change as required for mega/controllino board mapping
+
+int pixySelect = 49; //change as required for mega/controllino board mapping
 
 
 
@@ -16,22 +26,67 @@ void setup() {
  Serial1.begin(9600); 
  Serial2.begin(9600);
 
+//  SPI.transfer(0x00); //SPI communication with pixy
 }
 
 void loop() {
 
   if (waitStage){
-    
+    //default state to always wait for input from host PC for allowing the feeder to load x balls as requested by host PC
+
+
   }
 
   if (feederStage){
 
   }
   if (scanStage){
-
+    //to capture and store 50 LDR frames and push for data verification stage
   }
   if(packStage){
 
   }
 
 }   
+
+
+
+void serialEvent1() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+   // Serial.println(inChar);
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    Serial.println(stringComplete);
+    }
+  }
+}
+
+
+
+
+void serialEvent1() {
+  while (Serial1.available()) {
+    char inChar = (char)Serial1.read();
+   // Serial.println(inChar);
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    Serial.println(stringComplete);
+    }
+  }
+}
+
+
+void serialEvent2() {
+  while (Serial2.available()) {
+    char inChar = (char)Serial2.read();
+   // Serial.println(inChar);
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    Serial.println(stringComplete);
+    }
+  }
+}
