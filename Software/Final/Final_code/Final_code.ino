ca74fa48e6eacfeffde784e646723e5a77121925
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-03-09 22:01:41 +0800

    Create Final_code.ino

---
+++ Software/Final/Final_code/Final_code.ino
@@ -0,0 +1,21 @@
+
+
+//state machine
+bool waitStage = true;
+bool fedderStage = false;
+bool scanStage = false;
+bool packStage = false;
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
