5b0c0214165853e7539fae7e9ae72cbbf1a27546
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2020-03-10 19:54:16 +0800

    Update dataCollection.ino

--- Software/Machine Learning/Arduino/dataCollection/dataCollection/dataCollection.ino
+++ Software/Machine Learning/Arduino/dataCollection/dataCollection/dataCollection.ino
@@ -1,3 +1,19 @@
+//Project Designer Drugs: Assembling Custom Dosed Personal Medicines
+//NUS ESP3903 project in pursuit of Bachelor of Engineering in Engineering Science
+//Primary Author and Inital Code by Gokul Nathan
+//
+//Author(s): Gokul Nathan
+//
+
+
+
+//single data frame for AI, initially set at 50
+int dataFrameRunning[1][50][12] = {{{0}}};
+//end data frame
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
